537572885a9d40d74b958b85cfa5740bad159f1f
shuja-git <shuja.devops@gmail.com>
2025-06-17 18:24:27 +0530

    data block demo

--- 02-resource/main.tf
+++ 02-resource/main.tf
@@ -7,3 +7,4 @@ resource "aws_instance" "test_instance" {
     Name = "my-test-instance"
   }
 }
+
---
+++ 04-Data/data.tf
@@ -0,0 +1,8 @@
+data "aws_instances" "test" {
+  instance_tags = {
+    Name = "my-test-instance"
+  }
+}
+output "print_id" {
+  value = data.aws_instances.test.public_ips
+}
