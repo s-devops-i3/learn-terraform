05a47ed77d4bf8a868e53ef1941be5617cd4e711
shuja-git <shuja.devops@gmail.com>
2025-06-17 22:41:50 +0530

    name passed as input to module

--- 07-module/demo/ec2.tf
+++ 07-module/demo/ec2.tf
@@ -2,5 +2,9 @@ resource "aws_instance" "instance" {
   ami                       =  "ami-09c813fb71547fc4f"
   instance_type             = "t3.micro"
   vpc_security_group_ids    = ["sg-08f41a3b66746e56a"]
-
+  tags = {
+    Name = var.Name
+  }
 }
+
+variable "Name" {}
--- 07-module/main.tf
+++ 07-module/main.tf
@@ -1,7 +1,9 @@
 module "ec2-test-1" {
   source = "./demo"
+  Name   = "test1"
 }
 
 module "ec2-test-2" {
   source = "./demo"
+  Name   = "test2"
 }
