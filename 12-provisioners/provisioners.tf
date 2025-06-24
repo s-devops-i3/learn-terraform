ba59f1868b6c9fdcc49aaf9fcb486fd0d2a2e273
shuja-git <shuja.devops@gmail.com>
2025-06-24 23:08:04 +0530

    provisioners demo

---
+++ 12-provisioners/provisioners.tf
@@ -0,0 +1,18 @@
+resource "aws_instance" "instance" {
+  ami = "ami-09c813fb71547fc4f"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = ["sg-08f41a3b66746e56a"]
+
+  connection {
+    type = "ssh"
+    user = "ec2-user"
+    host = self.public_ip
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+
+      "echo Hello welcome to provisioners",
+    ]
+  }
+}
