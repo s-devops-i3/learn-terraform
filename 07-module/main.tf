ba16e0bfc671382ee00f4669568e83a78bc05dfd
shuja-git <shuja.devops@gmail.com>
2025-06-24 15:48:31 +0530

    module output testing

--- 07-module/main.tf
+++ 07-module/main.tf
@@ -3,10 +3,10 @@ module "ec2-test-1" {
   Name   = "test1"
 }
 
-module "ec2-test-2" {
-  source = "./demo"
-  Name   = "test2"
-}
+# module "ec2-test-2" {
+#   source = "./demo"
+#   Name   = "test2"
+# }
 
 output "ec2-test-1" {
   value = module.ec2-test-1
